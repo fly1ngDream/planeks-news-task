d068869f9af8f73502012e0dbb3b2f8a5cab23b8
Yevhen Shymotyuk <fly1ngDream@protonamil.com>
2019-09-08 17:16:32 +0300

    Update prod settings

--- django/config/settings/prod.py
+++ django/config/settings/prod.py
@@ -13,9 +13,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'planeks-news',
+        'NAME': 'planeks_news',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
